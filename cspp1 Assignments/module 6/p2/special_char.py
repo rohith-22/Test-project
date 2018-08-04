cb862cef40bbf62f481d7c10cfed881aea73624b
rohith-22 <rohithkottauppari@msitprogram.net>
2018-08-04 12:55:30 +0530

    " -> 7 of 7 passed. pylint: 8.00/10 "

--- cspp1 Assignments/module 6/p2/special_char.py
+++ cspp1 Assignments/module 6/p2/special_char.py
@@ -4,19 +4,14 @@ example : ab!@#cd is the input, the output is ab   cd
 Output has three spaces, which are to be replaced with these special characters
 '''
 def main():
-    str_i = input()
-    q = len(str_i)
-    str_r = ""
-    str_p = ""
-    for k in range(q):
-        print("1")
-        if str_i[k] == '!':
-            print(str_i[k])
-            str_p = str_r + " "
+    s_str = input()
+    z_str = ""
+    for char in s_str:
+        if char in "!@#$%^&*":
+            z_str +=" "
         else:
-        	print("2")
-        	str_p = str_r + str_i[k]
-        k += 1
-    print(str_p)
+            z_str += char
+    print(z_str)
+
 if __name__ == "__main__":
     main()

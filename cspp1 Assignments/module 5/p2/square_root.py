74db329ce97308344cbbc9945ee508d6a69a2434
rohith-22 <rohithkottauppari@msitprogram.net>
2018-08-05 21:26:15 +0530

    " -> 0 of 1 passed. pylint: 0.83/10 "

--- cspp1 Assignments/module 5/p2/square_root.py
+++ cspp1 Assignments/module 5/p2/square_root.py
@@ -1,9 +1,15 @@
 # Write a python program to find the square root of the given number 
 # using approximation method
-
 def main():
-	s = raw_input()
-	#your code here
-
+    VAR_EPSILON = 0.01
+    VAR_STEP = 0.1
+    VAR_SQUARE = int(input())
+    VAR_GUESS = 0.0
+    while VAR_GUESS <= VAR_SQUARE:
+        if abs(VAR_GUESS**2-VAR_SQUARE) < VAR_EPSILON:
+            break
+        else:
+            VAR_GUESS += VAR_STEP
+    print(str(VAR_GUESS))
 if __name__== "__main__":
 	main()

cae1d84b0c570c457b953724abddb038f6ff9ab5
rohith-22 <rohithkottauppari@msitprogram.net>
2018-08-05 21:51:38 +0530

    " -> 3 of 3 passed. pylint: 8.57/10 "

--- cspp1 Assignments/module 5/p3/square_root_bisection.py
+++ cspp1 Assignments/module 5/p3/square_root_bisection.py
@@ -1,9 +1,17 @@
-# Write a python program to find the square root of the given number 
+# Write a python program to find the square root of the given number
 # using Bi-section method
-
 def main():
-	s = raw_input()
-	#your code here
-
-if __name__== "__main__":
-	main()
+    num_a = int(input())
+    var_epsilon = 0.01
+    var_low = 0.0
+    var_high = num_a
+    bi_val = (var_high + var_low)/2.0
+    while abs(bi_val**2 - num_a) > var_epsilon:
+        if bi_val**2 < num_a:
+            var_low = bi_val
+        else:
+            var_high = bi_val
+        bi_val = (var_high + var_low)/2.0
+    print(bi_val)
+if __name__ == "__main__":
+    main()

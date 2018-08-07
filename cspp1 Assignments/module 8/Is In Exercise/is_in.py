649a640d616cf81ad1831cfd90ec8ac7f6424f6a
rohith-22 <rohithkottauppari@msitprogram.net>
2018-08-08 00:04:48 +0530

    is_ny

--- cspp1 Assignments/module 8/Is In Exercise/is_in.py
+++ cspp1 Assignments/module 8/Is In Exercise/is_in.py
@@ -1,23 +1,36 @@
 # Exercise: Is In
-# Write a Python function, isIn(char, aStr), that takes in two arguments a character and String and returns the isIn(char, aStr) which retuns a boolean value.
+# Write a Python function, isIn(cha_r, g_str), that takes in two arguments a cha_racter and String and returns the isIn(cha_r, g_str) which retuns a boolean value.
 
-# This function takes in two arguments character and String and returns one boolean value.
+# This function takes in two arguments cha_racter and String and returns one boolean value.
 
-def isIn(char, aStr):
+def isIn(cha_r, g_str):
     '''
-    char: a single character
-    aStr: an alphabetized string
+    cha_r: a single cha_racter
+    g_str: an alphabetized string
     
-    returns: True if char is in aStr; False otherwise
+    returns: True if cha_r is in g_str; False otherwise
     '''
-    # Your code here
-   
-
+    a = None
+    cha_r = cha_r.lower()
+    s_str = g_str.lower()
+    b_str = ''.join(sorted(s_str))
+    if len(b_str) == 0:
+        return "String length cannot be 0."
+    elif len(b_str) == 1:
+        if cha_r in b_str:
+            return True
+        return False
+    else:
+        a = int(len(b_str)/2)
+        if cha_r == b_str[a]:
+            return True
+        else:
+            if cha_r < b_str[a]:
+                return isIn(cha_r,b_str[:a])
+            return isIn(cha_r,b_str[a:])
 def main():
     data = input()
     data = data.split()
     print(isIn((data[0][0]), data[1]))
-
-
 if __name__== "__main__":
     main()

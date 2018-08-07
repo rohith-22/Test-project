88a6236687831cb43c2d148216e272d0fd5cc933
rohith-22 <rohithkottauppari@msitprogram.net>
2018-08-07 12:31:39 +0530

    using iteration method

---
+++ cspp1 Assignments/module 8/power using Iteration/power_iter.py
@@ -0,0 +1,24 @@
+'''# Exercise: PowerIter
+# Write a Python function, iterPower(var_base, var_exp),
+that takes in two numbers and returns the var_base^(var_exp) of given var_base and var_exp.
+# This function takes in two numbers and returns one number.'''
+def var_iterpower(var_base, var_exp):
+    '''
+    var_base: int or float.
+    var_exp: int >= 0
+    returns: int or float, var_base^var_exp
+    '''
+    var_p = 1
+    if var_exp == 0:
+        return 1
+    while var_exp > 0:
+        var_p = var_p * var_base
+        var_exp -= 1
+    return var_p
+def main():
+    ''' main function '''
+    data = input()
+    data = data.split(' ')
+    print(var_iterpower(float(data[0]), int(data[1])))
+if __name__ == "__main__":
+    main()

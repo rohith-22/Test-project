038f4b286b5d950935e14b87fdcaf28583957fd2
rohith-22 <rohithkottauppari@msitprogram.net>
2018-08-07 23:35:54 +0530

    " -> 10 of 10 passed. pylint: 10.00/10 "

--- cspp1 Assignments/module 7/Functions - Assignment-2/assignment2.py
+++ cspp1 Assignments/module 7/Functions - Assignment-2/assignment2.py
@@ -1,49 +1,47 @@
-# Assignment-2 - Paying Debt off in a Year
+"""# Assignment-2 - Paying Debt off in a Year
 
-# Now write a program that calculates the minimum fixed monthly payment needed in order pay off a credit card balance within 12 months.
-# By a fixed monthly payment, we mean a single number which does not change each month, but instead is a constant amount that will be
+# Now write a program that calculates the minimum fixed monthly payment needed
+in order pay off a credit card balance within 12 months.
+# By a fixed monthly payment, we mean a single number which does not change each month,
+but instead is a constant amount that will be
 # paid each month.
-
 # In this problem, we will not be dealing with a minimum monthly payment rate.
-
 # The following variables contain values as described below:
 # balance - the outstanding balance on the credit card
 # annualInterestRate - annual interest rate as a decimal
-
-# The program should print out one line: the lowest monthly payment that will pay off all debt in under 1 year, for example:
+# The program should print out one line: the lowest monthly payment that
+will pay off all debt in under 1 year, for example:
 # Lowest Payment: 180
-
-# Assume that the interest is compounded monthly according to the balance at the end of the month (after the payment for that month is
-# made).
-# The monthly payment must be a multiple of $10 and is the same for all months. Notice that it is possible for the balance to become
+# Assume that the interest is compounded monthly according tothe balance at the end of the month
+(after the payment for that month is made).
+# The monthly payment must be a multiple of $10 and is the same for all months.
+Notice that it is possible for the balance to become
 # negative using this payment scheme, which is okay. A summary of the required math is found below:
 # Monthly interest rate = (Annual interest rate) / 12.0
 # Monthly unpaid balance = (Previous balance) - (Minimum fixed monthly payment)
-# Updated balance each month = (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance)
-
-
-
+# Updated balance each month = (Monthly unpaid balance)
++(Monthly interest rate x Monthly unpaid balance)"""
 def paying_debtoffinayear(bal_ance, annualinterest_rate):
-   """defining paying function"""
-   var_bal = bal_ance
-   var_pay = 0
-   while True:
-       i = 12
-       var_bal = bal_ance
-       while i != 0:
-           var_ubal = var_bal - var_pay
-           var_bal = var_ubal + ((annualinterest_rate / 12.0) * var_ubal)
-           i -= 1
-       if var_bal > 0:
-           var_pay += 10
-       else:
-           break
-   return var_pay
+    """defining paying function"""
+    var_bal = bal_ance
+    var_pay = 0
+    while True:
+        i = 12
+        var_bal = bal_ance
+        while i != 0:
+            var_ubal = var_bal - var_pay
+            var_bal = var_ubal + ((annualinterest_rate / 12.0) * var_ubal)
+            i -= 1
+        if var_bal > 0:
+            var_pay += 10
+        else:
+            break
+    return var_pay
 def main():
-   """ main"""
-   data = input()
-   data = data.split(' ')
-   data = list(map(float, data))
-   print("Lowest Payment: " + str(paying_debtoffinayear(data[0], data[1])))
+    """ main"""
+    data = input()
+    data = data.split(' ')
+    data = list(map(float, data))
+    print("Lowest Payment: " + str(paying_debtoffinayear(data[0], data[1])))
 if __name__ == "__main__":
-   main()
+    main()

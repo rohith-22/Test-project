2f8dfeea82b8f05753b945cba74d7ec1e869d712
rohith-22 <rohithkottauppari@msitprogram.net>
2018-08-08 21:15:14 +0530

    " -> 9 of 9 passed. pylint: 10.00/10 "

--- cspp1 Assignments/module 9/p2/assignment2.py
+++ cspp1 Assignments/module 9/p2/assignment2.py
@@ -5,31 +5,34 @@ a string, secret_word, and a list of letters, letters_guessed. This function
 returns a string that is comprised of letters and underscores, based on what
 letters in letters_guessed are in secret_word. This shouldn't be too different from isWordGuessed!
 '''
-def get_guessed_word(secret_word, letters_guessed):
+def get_guessed_word(s_ecret_word, letters_guessed):
     '''
-    secret_word: string, the word the user is guessing
+    s_ecret_word: string, the word the user is guessing
     letters_guessed: list, what letters have been guessed so far
     returns: string, comprised of letters and underscores that represents
-      what letters in secret_word have been guessed so far.
+      what letters in s_ecret_word have been guessed so far.
     '''
     # FILL IN YOUR CODE HERE...
-    pass
+    for i in s_ecret_word:
+        if i not in letters_guessed:
+            s_ecret_word = s_ecret_word.replace(i, "_")
+    return s_ecret_word
 
 def main():
     '''
     Main function for current assignment
     '''
-    user_input = input()
-    if user_input:
-        data = user_input.split()
-        secret_word = data[0]
+    usr_inpt = input()
+    if usr_inpt:
+        data = usr_inpt.split()
+        s_ecret_word = data[0]
     else:
         data = []
-        secret_word = ""
+        s_ecret_word = ""
     list1 = []
     for j in range(1, len(data)):
         list1.append(data[j][0])
-    print(get_guessed_word(secret_word, list1))
+    print(get_guessed_word(s_ecret_word, list1))
 
 if __name__ == "__main__":
     main()

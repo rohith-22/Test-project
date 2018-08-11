99ffdb1955524cf00793f0b9e846a554e33df5ba
rohith-22 <rohithkottauppari@msitprogram.net>
2018-08-11 10:54:24 +0530

    " -> 0 of 3 passed. pylint: 9.05/10 "

--- cspp1 Assignments/module 12/p1/create_social_network.py
+++ cspp1 Assignments/module 12/p1/create_social_network.py
@@ -37,10 +37,11 @@ def create_social_network(data):
     s_dict = {}
     for i in range(len(var_a)):
         var_b = var_a[i].split(' follows ')
+        var_c = var_b[1].split(',')
         if var_b[0] not in s_dict:
-            s_dict[var_b[0]] = [var_b[1]]
-        if var_b[1] not in s_dict[var_b[0]]:
-            s_dict[var_b[0]].append(var_b[1])
+            s_dict[var_b[0]] = [var_c]
+        if var_c not in s_dict[var_b[0]]:
+            s_dict[var_b[0]].append(var_c)
     return(s_dict)
 def main():
     '''

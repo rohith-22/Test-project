99482ae029204370b7b352f24458641950fdcd7a
rohith-22 <rohithkottauppari@msitprogram.net>
2018-08-11 12:28:19 +0530

    " -> 0 of 2 passed. pylint: 9.64/10 "

--- cspp1 Assignments/module 12/p2/social_network.py
+++ cspp1 Assignments/module 12/p2/social_network.py
@@ -28,7 +28,7 @@ def unfollow(network, arg1, arg2):
     '''
     # remove the pass below and start writing your code
     if arg2 in network(arg1):
-        arg1.remove(arg2)
+        arg1.delet(arg2)
     return arg1
 
 def delete_person(network, arg1):
@@ -43,7 +43,7 @@ def delete_person(network, arg1):
     '''
     # remove the pass below and start writing your code
     if arg1 in network:
-       del network.arg1
+        network.delet(arg1)
     return arg1
 def main():
     '''

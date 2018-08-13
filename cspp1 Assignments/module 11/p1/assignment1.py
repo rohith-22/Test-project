aad09714622190003569a9575ae3d3dc356b110c
rohith-22 <rohithkottauppari@msitprogram.net>
2018-08-13 10:18:54 +0530

    assignment1

--- cspp1 Assignments/module 11/p1/assignment1.py
+++ cspp1 Assignments/module 11/p1/assignment1.py
@@ -1,9 +1,10 @@
 '''
 Exercise: Assignment-1
-The first step is to implement some code that allows us to calculate the score for a single word. The function get_word_score should accept as input a string of lowercase letters (a word) and return the integer score for that word, using the game's scoring rules.
+The first step is to implement some code that allows us to calculate the score for a single word.
+The function get_word_score should accept as input a string of lowercase letters (a word)
+and return the integer score for that word,using the game's scoring rules.
 '''
-
-def get_word_score(word, n):
+def get_word_score(word, var_n):
     """
     Returns the score for a word. Assumes the word is a valid word.
 
@@ -12,27 +13,26 @@ def get_word_score(word, n):
     letters are used on the first turn.
 
     Letters are scored as in Scrabble; A is worth 1, B is worth 3, C is
-    worth 3, D is worth 2, E is worth 1, and so on (see SCRABBLE_LETTER_VALUES)
+    worth 3, D is worth 2, E is worth 1, and so on (see scrabble_letter_values)
 
     word: string (lowercase letters)
     n: integer (HAND_SIZE; i.e., hand size required for additional points)
     returns: int >= 0
     """
-    SCRABBLE_LETTER_VALUES = {
-    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1,
-    'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
-    's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
+    scrabble_letter_values = {
+        'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1,
+        'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
+        's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
     }
     sum_0 = 0
-    L=len(word)
+    var_l = len(word)
     for j in word:
-        if j in SCRABBLE_LETTER_VALUES:
-            sum_0 = sum_0 + SCRABBLE_LETTER_VALUES[j]
-    if n == L:
-        sum_0 = sum_0*L
+        if j in scrabble_letter_values:
+            sum_0 = sum_0 + scrabble_letter_values[j]
+    if var_n == var_l:
+        sum_0 = sum_0*var_l
         return sum_0 + 50
-   
-    return sum_0 * L
+    return sum_0 * var_l
 def main():
     '''
     Main function for the given problem
@@ -40,7 +40,5 @@ def main():
     data = input()
     data = data.split()
     print(get_word_score(data[0], int(data[1])))
-
-
 if __name__ == "__main__":
     main()

52e8eba998c01a0504a9f022cb3ac6a2769ed64d
rohith-22 <rohithkottauppari@msitprogram.net>
2018-08-22 19:21:50 +0530

    " -> 7 of 7 passed. pylint: 9.29/10 "

--- cspp1 Assignments/module 6/p3/digit_product.py
+++ cspp1 Assignments/module 6/p3/digit_product.py
@@ -6,10 +6,16 @@ example:
 '''
 def main():
     int_input = int(input())
-    str_i = str(int_input)
+    int_input_a = int_input
+    if int_input < 0:
+        int_input_a = -int_input
+    str_i = str(int_input_a)
     product = 1
     for i in str_i:
         product = product * int(i)
-    print(product)
+    if int_input < 0:
+        print(-product)
+    else:
+        print(product)
 if __name__ == "__main__":
     main()

d3e92d02b9d5137fc5b4498d953a35791b2ccea8
rohith-22 <rohithkottauppari@msitprogram.net>
2018-08-25 13:09:00 +0530

    " -> 14 of 26 passed. pylint: 8.15/10 "

--- cspp1 Assignments/module 22/Check Sudoku/check_sudoku.py
+++ cspp1 Assignments/module 22/Check Sudoku/check_sudoku.py
@@ -7,7 +7,8 @@
     Complete the check_sudoku function to check if the given grid
     satisfies all the sudoku rules given in the statement above.
 '''
-
+# def check_r_c(sub_sudoku):
+#     for each in sub_sudoku:
 def check_sudoku(sudoku):
     '''
         Your solution goes here. You may add other helper functions as needed.
@@ -28,10 +29,66 @@ def check_sudoku(sudoku):
             for each in sudoku_column:
                 if each.count(each_element) > 1:
                     return False
+    sub_sudoku_1 = []
+    sub_sudoku_2 = []
+    sub_sudoku_3 = []
+    sub_sudoku_4 = []
+    sub_sudoku_5 = []
+    sub_sudoku_6 = []
+    sub_sudoku_7 = []
+    sub_sudoku_8 = []
+    sub_sudoku_9 = []
+    for i in range(3):
+        for j in range(3):
+            sub_sudoku_1.append(sudoku[i][j])
+        for each in sub_sudoku_1:
+            if sub_sudoku_1.count(each) > 1:
+                return False
+        for j in range(3 ,6):
+            sub_sudoku_2.append(sudoku[i][j])
+        for each in sub_sudoku_2:
+            if sub_sudoku_2.count(each) > 1:
+                return False
+        for j in range(6, 9):
+            sub_sudoku_3.append(sudoku[i][j])
+        for each in sub_sudoku_3:
+            if sub_sudoku_3.count(each) > 1:
+                return False
+    for i in range(3,6):
+        for j in range(3):
+            sub_sudoku_4.append(sudoku[i][j])
+        for each in sub_sudoku_4:
+            if sub_sudoku_4.count(each) > 1:
+                return False
+        for j in range(3 ,6):
+            sub_sudoku_5.append(sudoku[i][j])
+        for each in sub_sudoku_5:
+            if sub_sudoku_5.count(each) > 1:
+                return False
+        for j in range(6, 9):
+            sub_sudoku_6.append(sudoku[i][j])
+        for each in sub_sudoku_6:
+            if sub_sudoku_6.count(each) > 1:
+                return False
+    for i in range(6,9):
+        for j in range(3):
+            sub_sudoku_7.append(sudoku[i][j])
+        for each in sub_sudoku_7:
+            if sub_sudoku_1.count(each) > 1:
+                return False
+        for j in range(3 ,6):
+            sub_sudoku_8.append(sudoku[i][j])
+        for each in sub_sudoku_8:
+            if sub_sudoku_8.count(each) > 1:
+                return False
+        for j in range(6, 9):
+            sub_sudoku_9.append(sudoku[i][j])
+        for each in sub_sudoku_9:
+            if sub_sudoku_9.count(each) > 1:
+                return False
+
+    # print(sub_sudoku_9)
     return True
-
-
-
 def main():
     '''
         main function to read input sudoku from console

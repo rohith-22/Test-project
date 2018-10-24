aad17f5b5bdd8278caebc9992bc5ec033d3475aa
rohith-22 <rohithkottauppari@msitprogram.net>
2018-10-24 15:09:31 +0530

    " -> 0 of 10 passed. pylint: 0/0 "

--- cspp1 Assignments/module 7/Functions - Assignment-3/abcd.py
+++ cspp1 Assignments/module 7/Functions - Assignment-3/abcd.py
@@ -1,35 +1,29 @@
-def var_payingdebtoffinayear(var_balance, var_annualinterestrate):
-    """
-    This function is using bi-section search
-    """
-    var_monthlyinterestrate = (var_annualinterestrate) / 12.0
-    var_monthlypaymentlowerbound = var_balance / 12
-    var_monthlypaymentupperbound = (var_balance * (1 + var_monthlyinterestrate)**12) / 12.0
-    var_newbalance = var_balance
-    var_epsilon = 0.0001
-    var_guess = (var_monthlypaymentlowerbound + var_monthlypaymentupperbound)/2
-    while True:
-        var_month = 1
-        while var_month <= 12:
-            var_newbalance = var_newbalance - var_guess
-            var_newbalance = var_newbalance + (var_monthlyinterestrate * var_newbalance)
-            var_month += 1
-        if var_newbalance > 0 and var_newbalance > var_epsilon:
-            var_monthlypaymentlowerbound = var_guess
-            var_newbalance = var_balance
-        elif var_newbalance < 0 and var_newbalance < -var_epsilon:
-            var_monthlypaymentupperbound = var_guess
-            var_newbalance = var_balance
-        else:
-            return var_guess
-        var_guess = (var_monthlypaymentlowerbound + var_monthlypaymentupperbound)/2
+def payingdebt_offin_a_year(balance, annual_interest_rate):
+    '''input 320000 0.2
+    output: 29157.09'''
+    previous_balance = balance
+    monthly_interest_rate = annual_interest_rate/12
+    monthly_payment_lowerbound = previous_balance/12
+    monthly_payment_upperbound = (previous_balance*(1+monthly_interest_rate)**12)/12.0
+    epsilon = 0.03
+    while abs(balance) > epsilon:
+        monthly_payment = (monthly_payment_lowerbound+monthly_payment_upperbound)/2
+        balance = previous_balance
+        for _ in range(12):
+            monthly_unpaid_balance = (balance) - (monthly_payment)
+            balance = (monthly_unpaid_balance) + (monthly_interest_rate * monthly_unpaid_balance)
+        if balance > epsilon:
+            monthly_payment_lowerbound = monthly_payment
+        elif balance < -epsilon:
+            monthly_payment_upperbound = monthly_payment
+    return str(round(monthly_payment, 2))
+
 def main():
-    """
-    This function is to call the above
-    """
+    ''' this functon is used to print the lowest payment value'''
     data = input()
+    # data = "4773 0.2"
     data = data.split(' ')
     data = list(map(float, data))
-    print("Lowest Payment: " + str(round(var_payingdebtoffinayear(data[0], data[1]), 2)))
+    print("Lowest Payment:", payingdebt_offin_a_year(data[0], data[1]))
+
 if __name__ == "__main__":
-    main()
